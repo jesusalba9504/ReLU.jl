4b21319d032db150dc5b6ff138f3a7ecad51f33a
jesusalba9504 <49214775+jesusalba9504@users.noreply.github.com>
2019-06-05 13:54:43 -0400

    listo

--- test/runthis.jl
+++ test/runthis.jl
@@ -8,6 +8,6 @@ sgd!(net, data[1],0; test_data = data[2])
 net = Network(784, 30, 10)
 
 println("Results for ReLU Function")
-sgd!(net, data[1],0; test_data = data[2])
+sgd!(net, data[1],1; test_data = data[2])
 
 @test 1 == 1
